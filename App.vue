9fe7759f0d636c2eb3ebc19866f9130667097715
DaShuaiGe1hao <103572244+DaShuaiGe1hao@users.noreply.github.com>
2024-01-05 15:11:08 +0800

    Update App.vue

--- App.vue
+++ App.vue
@@ -2,6 +2,22 @@
 	export default {
 		onLaunch: function() {
 			console.log('App Launch')
+			// const value = uni.getStorageSync('launchFlag');
+			// if (value) {
+			// 	// 如何已经有，直接去首页
+			// 	uni.switchTab({
+			// 		url: '/pages/index/index'
+			// 	});
+			// } else {
+			// 	// 没有值，跳到引导页，并存储，下次打开就不会进去引导页
+			// 	uni.setStorage({
+			// 		key: 'launchFlag',
+			// 		data: true
+			// 	});
+			// 	uni.redirectTo({
+			// 		url: '/pages/index/guide'
+			// 	});
+			// }
 		},
 		onShow: function() {
 			console.log('App Show')
