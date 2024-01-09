5c06d5a55072543024ba62802b1734689ab82ca6
wqq <1786255766@qq.com>
2024-01-09 10:08:38 +0800

    我的页面

---
+++ pages/person/person/index.vue
@@ -0,0 +1,197 @@
+<template>
+	<view class="custom-navigation-bar" style="background-color: whitesmoke;">
+		<view class="left-button" @click="handleBack">
+			<uni-icons type="back" size="25"></uni-icons>
+		</view>
+		<view class="title">
+		</view>
+		<view class="right">
+			<!-- <view class="right-icon" @tap="toggle('center')">
+				<uni-icons type="search" size="25"></uni-icons>
+			</view> -->
+			<view class="right-icon">
+				<uni-search-bar @confirm="search" :focus="true" v-model="searchValue" @blur="blur" @focus="focus"
+					@input="input" @clear="clear" @cancel="cancel">
+				</uni-search-bar>
+			</view>
+			<view class="right-icon">
+				<uni-link href="https://uniapp.dcloud.io/" text="https://uniapp.dcloud.io/">
+					<uni-icons type="plusempty" size="25"></uni-icons>
+				</uni-link>
+			</view>
+			<view class="right-icon">
+				<uni-icons type="list" size="25"></uni-icons>
+			</view>
+		</view>
+	</view>
+
+	<view>
+		<view class="selection-operate">
+			<swiper class="sele-tab">
+				<swiper-item :class="{ 'sele-active': index == tabCur }" v-for="(item, index) in tabList" :key="index"
+					class="sele-item" @click="clickCtTab(index)">
+					<text v-text="item.title" class="sele-tab-item"></text>
+				</swiper-item>
+			</swiper>
+			<view v-if="tabCur===0">
+			</view>
+			<view v-if="tabCur===1">
+			</view>
+			<view v-if="tabCur===2">
+			</view>
+		</view>
+	</view>
+
+	<view class="sort-selections">
+		<uni-data-select v-model="value" :localdata="range" @change="change" placeholder="请选择排序方式"></uni-data-select>
+	</view>
+	<!-- 	<uni-popup ref="popcenter">
+		<view class="pop">
+			<uni-search-bar @confirm="search" :focus="true" v-model="searchValue" @blur="blur" @focus="focus"
+				@input="input" @clear="clear" @cancel="cancel">
+			</uni-search-bar>
+		</view>
+	</uni-popup> -->
+	<!-- 搜索弹出 -->
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				tabCur: 0,
+				tabList: [{
+					title: '我的文件夹',
+				}, {
+					title: '最近编辑',
+				}, {
+					title: '最近导出',
+				}],
+				value: 0,
+				range: [{
+						value: 0,
+						text: "创建时间"
+					},
+					{
+						value: 1,
+						text: "发布时间"
+					},
+					{
+						value: 2,
+						text: "文件名"
+					},
+					{
+						value: 3,
+						text: "状态"
+					},
+				],
+			}
+		},
+		props: {},
+		methods: {
+			handleBack() {
+				uni.navigateBack()
+			},
+			// toggle() {
+			// 	this.$refs['popcenter'].open();
+			// },
+			search(res) {
+				uni.showToast({
+					title: res.value,
+					icon: 'none'
+				})
+			},
+			input(res) {
+				console.log('input:', res)
+			},
+			clear(res) {
+				uni.showToast({
+					title: res.value,
+					icon: 'none'
+				})
+			},
+			cancel(res) {
+				uni.showToast({
+					title: res.value,
+					icon: 'none'
+				})
+			},
+			clickCtTab(ctCur) {
+				this.tabCur = ctCur
+
+			}
+		}
+	}
+</script>
+
+<style>
+	.custom-navigation-bar {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		height: 44px;
+	}
+
+	.left-button,
+	.right {
+		display: flex;
+		align-items: center;
+		padding: 0 12px;
+	}
+
+	.right-icon {
+		padding-right: 13px;
+	}
+
+	.selection-operate {
+		display: flex;
+		padding-top: 10px;
+	}
+
+	.sele-tab {
+		height: 50px;
+		margin-left: 20px;
+		padding: 0;
+		font-size: 26upx;
+		font-weight: bold;
+		color: #c0c8d0;
+		white-space: nowrap;
+		display: flex;
+		overflow: hidden;
+	}
+
+	.sele-item {
+		padding: 10px 18px;
+		display: inline-block;
+	}
+
+	.sele-item text {
+		padding: 5px 0;
+	}
+
+	.sele-active {
+		color: #007AFF;
+	}
+
+	.sele-active text {
+		border-bottom: 2px solid #007AFF;
+	}
+
+	swiper {
+		width: 100%;
+	}
+
+	swiper-item {
+		width: 150upx !important;
+	}
+
+	.sele-tab-item {
+		padding-left: 25px;
+	}
+
+	.sort-selections {
+		padding-left: 15px;
+		width: 120px;
+
+	}
+</style>
